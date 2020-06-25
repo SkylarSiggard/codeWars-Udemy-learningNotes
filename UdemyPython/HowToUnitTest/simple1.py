bce677cb86f0c5438eb4464a90811926b5e11b93
unknown <siggardskylar@gmail.com>
2020-06-24 19:25:53 -0600

    unit testing

---
+++ UdemyPython/HowToUnitTest/simple1.py
@@ -0,0 +1,20 @@
+# unit testing with pylint. i installed this by doing pip install pylint
+#? to run it 
+#! pylint simple1.py -r y
+
+a = 1 
+b = 2 
+print(a)
+print(b)
+
+def myfunc():
+    '''
+    A simple function 
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+myfunc() 
+
+
