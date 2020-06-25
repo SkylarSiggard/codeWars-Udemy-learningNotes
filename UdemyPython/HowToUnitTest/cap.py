3df4fa2eac52a1132362e616fb86b839b06e5a63
unknown <siggardskylar@gmail.com>
2020-06-24 19:35:15 -0600

    unit testing is pretty cool

---
+++ UdemyPython/HowToUnitTest/cap.py
@@ -0,0 +1,9 @@
+def cap_text(text):
+    '''
+    Input will return a string that is upper case 
+    '''
+    return text.title()  
+
+
+#!  capitalize() will upper the fist lettter of word
+#!  .title() will uppper every fist lettter of of each word 
