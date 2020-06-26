c67f2a02dcad5a961c82c5c55111f32fff04fab1
skylar siggard <siggardskylar@gmail.com>
2020-06-26 11:47:44 -0600

    i finished on unit test!

--- UdemyPython/Projects/BlackJackProject/blackjack_tests.py
+++ UdemyPython/Projects/BlackJackProject/blackjack_tests.py
@@ -4,9 +4,8 @@ import gameprogress
 class Testgameplay(unittest.TestCase):
     
     def test_inputs(self):
-        test = 1
-        result = cap.cap_text(test) 
-        self.assertEqual(result, 1)
+        result = gameprogress.makebet() 
+        self.assertTrue(result, result in [1,2,3,4,5,6,7,8,9,10])
 
 
 
@@ -14,11 +13,3 @@ if __name__ == "__main__":
     unittest.main() 
 
 
-
-def makebet():
-	choice = "Wrong"
-	while choice not in ['1','2','3','4','5','6','7','8','9','10']:
-		choice = input("\nMake a bet? \n")
-		if choice not in [1,2,3,4,5,6,7,8,9,10]:
-			print("Sorry, You need to bet at least $1 \n")	
-	return int(choice)
