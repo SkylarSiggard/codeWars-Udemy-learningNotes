94604397e5032b8b573e5ed6ec9277ba4be4662c
unknown <siggardskylar@gmail.com>
2020-08-12 15:16:21 -0600

    done

--- UdemyPython/Projects/BlackJackRedo/blackjackredo.py
+++ UdemyPython/Projects/BlackJackRedo/blackjackredo.py
@@ -1,5 +1,8 @@
 import random 
 
+#! this is the better version of what i ended up doing. Some things that can be added: save your chips for another round so you keep want you get 
+
+
 suits = ("Hearts","Diamonds","Spades","Clubs")
 ranks = ("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace")
 values = {"Two":1,"Three":3,"Four":4,"Five":5,"Six":6,"Seven":7,"Eight":8,"Nine":9,"Ten":10,"Jack":10,"Queen":10,"King":10,"Ace":11}
